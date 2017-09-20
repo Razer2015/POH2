bc461141ebe473b0bea0eeac6014dbfff2d53adc
Razer2015 <razer138@gmail.com>
2017-09-20 16:45:32 +0300

    - Harjoitus 3

--- POH2Luokat/Osasto.cs
+++ POH2Luokat/Osasto.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace POH2Luokat
 {
+    public delegate void PalkkaaminenHandler(object sender, Tyontekija t, CancelEventArgs e);
+    public delegate void ErottaminenHandler(object sender, Tyontekija t, EventArgs e);
+
     public class Osasto : IId, INimi
     {
+        public event PalkkaaminenHandler Palkkaaminen;
+        public event ErottaminenHandler Erottaminen;
+
         public int Id { get; private set; }
         public string Nimi { get; set; }
         public List<Tyontekija> Tyontekijat { get; set; }
@@ -23,10 +27,22 @@ namespace POH2Luokat
         }
 
         public override string ToString() {
-            return ($"{this.Nimi} ({this.HenkiloLkm})");
+            var paluu = ($"{this.Nimi} ({this.HenkiloLkm})");
+            paluu += Palkkaaminen != null ? " +" : "";
+            paluu += Erottaminen != null ? " -" : "";
+            return (paluu);
         }
 
         public void Palkkaa(Tyontekija tyontekija, double palkka) {
+            if(Palkkaaminen != null) {
+                var cancel = new CancelEventArgs();
+                Palkkaaminen(this, tyontekija, cancel);
+
+                if (cancel.Cancel.Equals(true)) {
+                    return;
+                }
+            }
+
             tyontekija.Palkka = palkka;
             tyontekija.PalkkausPvm = DateTime.Now;
             this.Tyontekijat.Add(tyontekija);
@@ -34,6 +50,11 @@ namespace POH2Luokat
         }
 
         public void Erota(Tyontekija tyontekija) {
+            if (Erottaminen != null) {
+                var eventArgs = new EventArgs();
+                Erottaminen(this, tyontekija, eventArgs);
+            }
+
             tyontekija.PaattymisPvm = DateTime.Now;
             this.Tyontekijat.Remove(tyontekija);
         }
--- POH2Testeri1/Program.cs
+++ POH2Testeri1/Program.cs
@@ -4,27 +4,86 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
+using System.IO;
 
 namespace POH2Testeri1
 {
     class Program
     {
         static void Main(string[] args) {
-            int tyontekijatLKM = 20;
-            var tyontekijat = Tyontekija.GeneroiData(tyontekijatLKM);
-            Console.WriteLine($"Luodaan {tyontekijatLKM} työntekijää...");
-            foreach (var tyontekija in tyontekijat) {
-                Console.WriteLine("{0}, ikä {1}", tyontekija.ToString(), tyontekija.Ika);
+            //int tyontekijatLKM = 20;
+            //var tyontekijat = Tyontekija.GeneroiData(tyontekijatLKM);
+            //Console.WriteLine($"Luodaan {tyontekijatLKM} työntekijää...");
+            //foreach (var tyontekija in tyontekijat) {
+            //    Console.WriteLine("{0}, ikä {1}", tyontekija.ToString(), tyontekija.Ika);
+            //}
+
+            //int osastoLKM = 5;
+            //Console.WriteLine($"\r\nLuodaan {osastoLKM} osastoa...");
+            //var osastot = Osasto.GeneroiData(osastoLKM);
+            //foreach (var osasto in osastot) {
+            //    Console.WriteLine(osasto.ToString());
+            //}
+
+            var osastot = Osasto.GeneroiData(4).ToArray();
+            var tyontekijat = Tyontekija.GeneroiData(80).ToArray();
+
+            osastot[0].Palkkaaminen += PalkkausKasittelija;
+            osastot[0].Erottaminen += ErottamisKasittelija;
+            osastot[1].Palkkaaminen += PalkkausKasittelija;
+            osastot[2].Erottaminen += ErottamisKasittelija;
+
+            // Palkataan 20 työntekijää joka osastoon
+            int index = 0;
+            for (int i = 0; i < osastot.Length; i++) {
+                for (int j = 0; j < tyontekijat.Length / osastot.Length; j++) {
+                    osastot[i].Palkkaa(tyontekijat[index++], 0);
+                }
             }
 
-            int osastoLKM = 5;
-            Console.WriteLine($"\r\nLuodaan {osastoLKM} osastoa...");
-            var osastot = Osasto.GeneroiData(osastoLKM);
-            foreach (var osasto in osastot) {
-                Console.WriteLine(osasto.ToString());
+            // Erotetaan sukunimi "nen" loppuiset
+            for (int i = 0; i < osastot.Length; i++) {
+                var t = osastot[i].Tyontekijat;
+                var erotettavat = t.FindAll(x => x.SukuNimi.EndsWith("nen"));
+                foreach (var erotettava in erotettavat) {
+                    osastot[i].Erota(erotettava);
+                }
+            }
+
+            // Tulostetaan osastojen tiedot
+            for (int i = 0; i < osastot.Length; i++) {
+                Console.WriteLine(osastot[i].ToString());
+                for (int j = 0; j < osastot[i].Tyontekijat.Count; j++) {
+                    Console.WriteLine(osastot[i].Tyontekijat[j].ToString());
+                }
+                Console.WriteLine();
             }
 
             Console.ReadKey();
+        }
+
+        private static void KirjoitaLokiin(string loki, string rivi) {
+            try {
+                using (var sw = new StreamWriter(loki, true)) {
+                    sw.WriteLine(rivi);
+                }
+            }
+            catch { }
+        }
+
+        private static void PalkkausKasittelija(object sender, Tyontekija t, CancelEventArgs e) {
+            KirjoitaLokiin("POH2PalkkausLoki.txt", 
+                $"{((Osasto)sender).Nimi} {DateTime.Now.ToString("dd.MM.yyyy")} {t.Nimi} {t.SyntymaAika.Value.ToString("dd.MM.yyyy")}");
+        }
+
+
+        private static void ErottamisKasittelija(object sender, Tyontekija t, EventArgs e) {
+            KirjoitaLokiin("POH2ErottamisLoki.txt", string.Format("{0} {1} {2} {3}",
+                ((Osasto)sender).Nimi,
+                DateTime.Now.ToString("dd.MM.yyyy"),
+                t.Nimi,
+                t.SyntymaAika.Value.ToString("dd.MM.yyyy")));
         }
     }
 }
